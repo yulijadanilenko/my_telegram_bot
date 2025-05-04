a57bfa75d75d3799b3c200ed8cba29e88aa10c3f
yulijadanilenko <yulijadanilenko@gmail.com>
2025-05-04 18:34:46 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Я работаю на Render!")
+
+print("Бот запущен...")
+bot.polling()
